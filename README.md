85d3d16a34332e2d22ce948f4e245765b1b09ba4
bisratdbelay <bisratdb@outlook.com>
2023-03-03 20:59:13 -0600

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # html_weather
 html_weather
+
+instructions.
+
+place files into public_html directory on your web server.
+
+to run program execute:
+
+./wget.sh
---
+++ wget.sh
@@ -0,0 +1,3 @@
+#enter your API key after appid tag
+
+wget -O index.html 'https://api.openweathermap.org/data/2.5/weather?appid=&units=metric&lat=32.779167&lon=-96.808891'
